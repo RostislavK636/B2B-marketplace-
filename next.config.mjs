d3752cf6866ba74a4eacf60d544e2e2d607f2ab3
RostislavK636 <rostislavkrivokolisko@gmail.com>
2025-11-29 01:40:21 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/B2B-marketplace-' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/B2B-marketplace-/' : '',
 }
 
 export default nextConfig
