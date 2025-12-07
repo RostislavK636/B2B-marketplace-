f88cde7fe6ff69bbae40842b82e02fec098ebf90
paulzverev <paulzvereve@gmail.com>
2025-12-07 12:59:54 +0300

    local configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,33 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   output: 'standalone', // Критически важно для Docker
+//   poweredByHeader: false,
+//   images: {
+//     unoptimized: true, // Для простоты, можно изменить позже
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true, // Игнорировать ошибки ESLint при сборке
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true, // Игнорировать ошибки TypeScript при сборке
+//   }
+// }
+
+
+// module.exports = nextConfig
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone', // Критически важно для Docker
-  poweredByHeader: false,
-  images: {
-    unoptimized: true, // Для простоты, можно изменить позже
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Игнорировать ошибки ESLint при сборке
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Игнорировать ошибки TypeScript при сборке
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ]
   }
 }
 
