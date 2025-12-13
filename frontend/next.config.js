b09e35633c0d7b553a09b09f66b17674515bb319
paulzverev <paulzvereve@gmail.com>
2025-12-13 15:56:34 +0300

    pre-deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,34 +1,34 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   output: 'standalone', // Критически важно для Docker
-//   poweredByHeader: false,
-//   images: {
-//     unoptimized: true, // Для простоты, можно изменить позже
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true, // Игнорировать ошибки ESLint при сборке
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true, // Игнорировать ошибки TypeScript при сборке
-//   }
-// }
-
-
-// module.exports = nextConfig
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
-      },
-    ]
+  swcMinify: true,
+  output: 'standalone', // Критически важно для Docker
+  poweredByHeader: false,
+  images: {
+    unoptimized: true, // Для простоты, можно изменить позже
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Игнорировать ошибки ESLint при сборке
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Игнорировать ошибки TypeScript при сборке
   }
 }
 
+
 module.exports = nextConfig
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: 'standalone',
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:8080/api/:path*',
+//       },
+//     ]
+//   }
+// }
+
+// module.exports = nextConfig
